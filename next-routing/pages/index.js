be97b4f3cf6962f4f4e203f4a71800b8b82008ac
Sasikant Biswal <biswalsashikant@gmail.com>
2022-11-19 16:16:49 +0530

    Added navigating programatically routing

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+  const router = useRouter();
+
+  // Navigating Programatically
+  const handleClick = () => {
+    console.log("Placing your order.");
+    // router.push("/product");
+    router.replace("/product");
+  };
+
   return (
     <div>
-      <Link href="/blog">
-        Blog
-      </Link>
-      <Link href="/product">
-        Products
-      </Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/product">Products</Link>
       <h1>Home Page</h1>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 };
