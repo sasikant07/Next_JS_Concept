af467b1c236b170d1339a400a5edf5ae89ffcc4d
Sasikant Biswal <biswalsashikant@gmail.com>
2022-11-20 18:28:04 +0530

    Added code for pages vs components

---
+++ next-pre-rendering/components/user.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const User = ({user}) => {
+  return (
+    <div>
+      <h6>Name: {user.name}</h6>
+      <h6>Email: {user.email}</h6>
+    </div>
+  );
+};
+
+export default User;
