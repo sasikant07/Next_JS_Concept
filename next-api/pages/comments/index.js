c09da2b7c7b6d16548e238b624a20e1ad6e01a80
Sasikant Biswal <biswalsashikant@gmail.com>
2023-01-14 19:26:11 +0530

    API POST Request

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 
 function CommentsPage() {
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments');
@@ -9,8 +10,22 @@ function CommentsPage() {
         setComments(data);
     }
 
+    const submitComment = async () => {
+        const response = await fetch('api/comments', {
+            method: 'POST',
+            body: JSON.stringify({comment}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const data = await response.json();
+        console.log(data);
+    }
+
     return (
         <>
+            <input type='text' value={comment} onChange={(e) => setComment(e.target.value)} />
+            <button onClick={submitComment}>Submit Comment</button>
             <button onClick={fetchComments}>Load Comments</button>
             {
                 comments.map(comment => {
