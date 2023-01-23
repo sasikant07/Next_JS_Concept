6f74c5f64ceb7e5b57206510ca76325152f85f0c
Sasikant Biswal <biswalsashikant@gmail.com>
2023-01-23 21:37:15 +0530

    Securing Pages Client-side

--- next-authentication/pages/dashboard.js
+++ next-authentication/pages/dashboard.js
@@ -1,4 +1,25 @@
+import {useState, useEffect} from 'react';
+import { getSession, signIn } from 'next-auth/react'
+
 function Dashboard() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const seCurePage = async () => {
+            const session = await getSession();
+            if (!session) {
+                signIn()
+            } else {
+                setLoading(false);
+            }
+        }
+        seCurePage();
+    }, [])
+
+    if (loading) {
+        return <h2>Loading...</h2>
+    }
+
     return <h1>Dashboard Page</h1>
 }
 
