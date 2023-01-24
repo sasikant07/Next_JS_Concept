3e0e60233ec93915f663d8b3600bcd85f03e1a3a
Sasikant Biswal <biswalsashikant@gmail.com>
2023-01-24 12:15:41 +0530

    NextAuth Provider

--- next-authentication/pages/_app.js
+++ next-authentication/pages/_app.js
@@ -5,12 +5,10 @@ import Navbar from '../components/Navbar'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
     <SessionProvider session={pageProps.session}>
         <Navbar />
         <Component {...pageProps} />
     </SessionProvider>
-    </>
   )
 }
 
--- next-authentication/pages/index.js
+++ next-authentication/pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useSession } from 'next-auth/react'
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  console.log("Session: ", session, "Status: ", status);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +18,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {session ? `${session.user.name},` : ''} Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
