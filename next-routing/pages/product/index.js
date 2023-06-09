4f7d19ab5da166a5c8a9b703c0ee755462ead3d1
Sasikant Biswal <biswalsashikant@gmail.com>
2023-06-09 13:16:42 +0530

    Added some comments

--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -12,7 +12,7 @@ const ProductList = ({productId = 100}) => {
         <Link href="/product/product2">Product 2</Link>
       </h2>
       <h2>
-        <Link href="/product/product3" replace>Product 3</Link>
+        <Link href="/product/product3" replace>Product 3</Link>   // replace: replace prop will replace the current history state instead of adding a new url to this tag 
       </h2>
       <h2>
         <Link href={`/product/${productId}`}>Product {productId}</Link>
